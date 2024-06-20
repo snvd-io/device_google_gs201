d5a7bdd2923cc8e72b4850a2e8888ac6668d7794
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-06-20 15:12:01 +0300

    improve error handling in USB port security API
    setPortSecurityState() is a oneway call, i.e. return value is not forwarded to the caller. This
    commit switches it to use a callback instead.

--- usb/usb/Usb.h
+++ usb/usb/Usb.h
@@ -107,14 +107,15 @@ struct Usb : public BnUsb {
     pthread_t mUsbHost;
 };
 
+using ext::IPortSecurityStateCallback;
 using ext::PortSecurityState;
 
 struct UsbExt : public ext::BnUsbExt {
     UsbExt(std::shared_ptr<Usb> usb);
 
-    ScopedAStatus setPortSecurityState(const std::string& in_portName,
-                                       PortSecurityState in_state) override;
-    ScopedAStatus setPortSecurityStateInner(PortSecurityState in_state);
+    ScopedAStatus setPortSecurityState(const std::string& in_portName, PortSecurityState in_state,
+            const shared_ptr<IPortSecurityStateCallback>& in_callback) override;
+    int setPortSecurityStateInner(PortSecurityState in_state);
 
     std::shared_ptr<Usb> mUsb;
 };
